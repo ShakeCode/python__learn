37be33b6ccd13b468e93cb9f1d632fea99fd32f7
ShakeCode <903936584@qq.com>
2022-10-05 18:00:26 +0800

    日志记录用例

--- venv/com/python/logging/logTest.py
+++ venv/com/python/logging/logTest.py
@@ -8,7 +8,6 @@ import logging
 %o，格式化无符号八进制数6、%f,浮点数（float）占位符，也可以表示整数（int），默认保留小数点后6位
 '''
 
-
 logFile = 'mylog.log'
 formatStr11 = '%(asctime)s - %(name)s - process_id: %(process)s - thread_id: %(thread)d - %(filename)s - %(levelname)s - %(lineno)d: %(message)s'
 formatStr22 = '%(asctime)s - %(name)s - %(levelname)s -%(module)s:  %(message)s'
@@ -19,20 +18,20 @@ logger = logging.getLogger()
 streamHandler = logging.StreamHandler()
 streamHandler.setFormatter(logging.Formatter(formatStr11))
 logger.addHandler(streamHandler)
-#fileHandler = logging.FileHandler(logFile, mode='a')
-#获取当前路径，绝对路径
-#path = os.path.abspath('.')
-#fileHandler = handlers.TimedRotatingFileHandler(filename=path+'\\send.log', when='D')
-#logger.addHandler(fileHandler)
+# fileHandler = logging.FileHandler(logFile, mode='a')
+# 获取当前路径，绝对路径
+# path = os.path.abspath('.')
+# fileHandler = handlers.TimedRotatingFileHandler(filename=path+'\\send.log', when='D')
+# logger.addHandler(fileHandler)
 
 logger.error('param url:{0}'.format('baidu.com'))
-logger.info("设备ID=%s ,  计划时间=%s分钟", 11, 5 )
-logger.info("这是数字%d",800)
-logger.info("这是小数%f",800)
-logger.info("这是无符号整数%u",800)
-logger.info("这是无符号八进制%o",21321)
+logger.info("设备ID=%s ,  计划时间=%s分钟", 11, 5)
+logger.info("这是数字%d", 800)
+logger.info("这是小数%f", 800)
+logger.info("这是无符号整数%u", 800)
+logger.info("这是无符号八进制%o", 21321)
 
-#另外用%s,也可输出其他非字符串的数据：
+# 另外用%s,也可输出其他非字符串的数据：
 
-logger.info("这是time 模块 %s",111)
-logger.info("这是threading 模块 %s",2222)
+logger.info("这是time 模块 %s", 111)
+logger.info("这是threading 模块 %s", 2222)
